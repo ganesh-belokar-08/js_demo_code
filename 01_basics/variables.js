84bbf6b427f4db8512f18eb55f4bf9e2bdc74293
Ganesh Belokar <ganeshbelokar08@gmail.com>
2025-01-27 12:27:32 +0000

    New things are added

---
+++ 01_basics/variables.js
@@ -0,0 +1,9 @@
+let studentName = "Ganesh ";
+let studentRoll = 08;
+let studentCity = "Pune, Maharashtra";
+
+//console.log
+console.log("studentName");
+
+//console.log
+console.table([studentName, studentRoll, studentCity]);
