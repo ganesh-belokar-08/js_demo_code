0de6492a324ac6647eae76a20a42d85fe75d25ee
Ganesh Belokar <ganeshbelokar08@gmail.com>
2025-01-27 15:03:11 +0000

    type Conversions

---
+++ 01_basics/conversion.js
@@ -0,0 +1,16 @@
+let number = 22;
+console.log(number);
+console.log(typeof(number));
+
+let age = "22ab"
+console.log(age);
+//parseInt(age);
+console.log(age);
+console.log(typeof(age));
+
+let isLoggedIn= true;
+let booleanLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(isLoggedIn));
+
+// 1=> True & 0=> False 
+// Ganesh=> True & " "=>False
