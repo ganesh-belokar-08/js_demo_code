34fa742ca856f9b43470f9a79eacaf621f94f240
Ganesh Belokar <ganeshbelokar08@gmail.com>
2025-01-27 15:59:27 +0000

    Operators

---
+++ 01_basics/04_string_to_number.js
@@ -0,0 +1,31 @@
+//   console.log(2*2);
+//   console.log(2**3);
+
+//   let str1 = "Hello";
+//   let str2 = " Ganesh";
+//   let str3 = str1 + str2
+//   console.log(str3);
+
+//   // String & Number
+//   console.log("1" + 2);
+//   console.log(1+"2");
+//   // String & String 
+//   console.log("1"+"2");
+//   // Number & Number & String
+//   console.log(1+2+"3");
+
+//   console.log("true");
+//   console.log("+true");
+
+//   console.log(+""); 
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+
+gameCounter++;
+//console.log(gameCounter);
+
+//gameCounter=+gameCounter;
+
+console.log(gameCounter);
