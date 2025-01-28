3d4ad416ce716aaa2f1e22d48c7c3ba9a0126067
Ganesh Belokar <ganeshbelokar08@gmail.com>
2025-01-28 08:30:15 +0000

    NESTED OBJECTS Handling

---
+++ 02_basics/04_object.js
@@ -0,0 +1,40 @@
+
+const user = [
+    {
+        username : "bravo",
+        email : "bravo@gmail.com"
+       },
+       {
+        username : "alpha",
+        email : "alpha@gmail.com"
+       }
+    
+]
+//console.log(users[bravo].email);
+
+const student = [
+    {
+        id : 1,
+        name : "Ganesh",
+        email : "ganesh@gmail.com"
+    },
+    {
+        id : 2,
+        name : "Rajesh",
+        email :"rajesh@gmail.com"
+    }
+]
+
+//console.log(user);
+//console.log(student);
+
+//console.log(student[1].name);
+
+// FOR STUDENT OBJECT
+ //console.log(Object.keys(student));
+  //console.log(Object.values(student));
+
+// FOR USER OBJECT
+// console.log(Object.keys(user));
+//console.log(Object.values(user));
+console.log(Object.entries(user));
